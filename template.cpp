3b8acd5bd92426dc27fd5c4dff89d0d3c3240073
Md. Hasib Khan <72490271+hasibx@users.noreply.github.com>
2025-07-20 15:07:34 +0600

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+const ll mod = 1000000007;
+
+#define f(i,a,b) for(int i=int(a);i<int(b);i++)
+#define fr(i,a,b) for(int i=int(a);i>int(b);i--)
+#define test int TTT; cin >> TTT; while(TTT--)
+#define cases int TTT; cin >> TTT; f(i,1,TTT+1) { cout << "Case " << i << ": ";
+#define endcases }
+#define pb push_back
+#define pii pair<int,int>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vcin(a,n) vector<ll> a(n); f(i,0,n) cin >> a[i];
+#define vvcin(matrix, n, m) vvi matrix(n, vector<int>(m)); f(i,0,n) f(j,0,m) cin >> matrix[i][j];
+#define vpi vector<pair<int,int>>
+#define mi map<int,int>
+#define pll pair<ll,ll>
+#define vll vector<ll>
+#define vpll vector<pair<ll,ll>>
+#define mll map<ll,ll>
+#define SORT(x) sort(x.begin(),x.end())
+#define ALL(x) (x).begin(), (x).end()
+#define YY cout << "YES\n"
+#define NN cout << "NO\n"
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); cout << fixed << setprecision(10);
+
+void io(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+int main() {
+    io();
+    fast; 
+
+    
+
+    return 0;
+}
