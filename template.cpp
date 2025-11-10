d383b971ad5ee18f596f2e42a9ac9656b5f06b5e
Md. Hasib Khan <mhk.txt@gmail.com>
2025-11-10 20:18:41 +0600

    Small Update

--- template.cpp
+++ template.cpp
@@ -2,19 +2,16 @@
 using namespace std;
 
 using ll = long long;
-const ll mod = 1000000007;
 
 #define f(i,a,b) for(int i=int(a);i<int(b);i++)
 #define fr(i,a,b) for(int i=int(a);i>int(b);i--)
 #define test int TTT; cin >> TTT; while(TTT--)
-#define cases int TTT; cin >> TTT; f(i,1,TTT+1) { cout << "Case " << i << ": ";
-#define endcases }
 #define pb push_back
 #define pii pair<int,int>
 #define vi vector<ll>
 #define vvi vector<vector<ll>>
 #define vcin(a,n) vector<ll> a(n); f(i,0,n) cin >> a[i];
-#define vvcin(matrix, n, m) vvi matrix(n, vector<char>(m)); f(i,0,n) f(j,0,m) cin >> matrix[i][j];
+#define vvcin(matrix, n, m) vvi matrix(n, vector<ll>(m)); f(i,0,n) f(j,0,m) cin >> matrix[i][j];
 #define vpi vector<pair<int,int>>
 #define mi map<int,int>
 #define pll pair<ll,ll>
@@ -24,8 +21,8 @@ const ll mod = 1000000007;
 #define SORT(x) sort(x.begin(),x.end())
 #define SORTR(x) sort(x.begin(), x.end(), greater<>())
 #define ALL(x) (x).begin(), (x).end()
-#define YY cout << "YES\n"
-#define NN cout << "NO\n"
+#define YY cout << "Y\n"
+#define NN cout << "N\n"
 #define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); cout << fixed << setprecision(10);
 
 void io(){
@@ -37,14 +34,12 @@ void io(){
 
 int main() {
  
-  io();
+  // io();
   fast;
  
 // Code
 
 
-
-  
 
   return 0;
 }
